fcc1a679fe6a481a300e68340cf7bee02800206c
a.22308051281092 <a.22308051281092@cbtis128.edu.mx>
2025-05-26 17:01:16 +0000

    first commit

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF9966CC);       // Morado amatista
+const secondaryColor = Color(0xFF98FF98);     // Verde menta
+const backgroundColor1 = Color.fromARGB(255, 227, 227, 231);   // Fondo claro
+const backgroundColor2 = Color.fromARGB(255, 193, 192, 194);   // Fondo más suave aún
+const lightSecondary = Color(0xFFD1FFD1);     // Verde menta claro (para hover o efectos sutiles)
---
+++ lib/model.dart
@@ -0,0 +1,37 @@
+const destination = [
+  {
+    'image': 'https://raw.githubusercontent.com/Alondra-Ramirez-Sandoval-1092/jinstar_imagenes/refs/heads/main/groupgrey.jpg'  
+  },
+  {
+    'image': 'https://raw.githubusercontent.com/Alondra-Ramirez-Sandoval-1092/jinstar_imagenes/refs/heads/main/groupgrey2.png'
+  },
+  {
+    'image': 'https://raw.githubusercontent.com/Alondra-Ramirez-Sandoval-1092/jinstar_imagenes/refs/heads/main/groupgrey3.jpg'
+  },
+  {
+    'image': 'https://raw.githubusercontent.com/Alondra-Ramirez-Sandoval-1092/jinstar_imagenes/refs/heads/main/groupgrey4.jpg'
+  },
+];
+
+const hotDestination = [
+   {
+    'image': 'https://raw.githubusercontent.com/Alondra-Ramirez-Sandoval-1092/jinstar_imagenes/refs/heads/main/addison.jpg',
+    'name': 'Ginecologa',
+    'noOfPlace': '20 Consultas Semanales',
+  },
+  {
+    'image': 'https://raw.githubusercontent.com/Alondra-Ramirez-Sandoval-1092/jinstar_imagenes/refs/heads/main/derek.jpg',
+    'name': 'Neurologo',
+    'noOfPlace': '18 Consultas Semanales',
+  },
+  {
+    'image': 'https://raw.githubusercontent.com/Alondra-Ramirez-Sandoval-1092/jinstar_imagenes/refs/heads/main/yangg.jpg',
+    'name': 'Cardiologa',
+    'noOfPlace': '15 Consultas Semanales ',
+  },
+  {
+    'image': 'https://raw.githubusercontent.com/Alondra-Ramirez-Sandoval-1092/jinstar_imagenes/refs/heads/main/mergrey.jpg',
+    'name': 'General',
+    'noOfPlace': '15 Consultas Semanales',
+  },
+];
